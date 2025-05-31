64afba6c9b46e7f02318d6ae19847aba41b41692
sandrorsjunior <sandrorsjunior@outlook.com>
2025-05-31 21:21:47 +0100

    fixed images

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,4 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   base: "/livia-portfolio",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
-    },
-  },
 });
